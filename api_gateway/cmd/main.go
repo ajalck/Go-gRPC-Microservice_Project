09eb676c3c83bb032af3dc4c4be7d8761bb6b215
Ajal C K <115230603+ajalck@users.noreply.github.com>
2023-05-13 17:02:15 +0530

    updations

--- api_gateway/cmd/main.go
+++ api_gateway/cmd/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"go-grpc-microservice-api_gateway/pkg/config"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hashicorp/go-hclog"
+)
+
+func main() {
+	log := hclog.Default()
+	log.Info("Starting api gateway")
+
+	c, err := config.LoadConfig()
+	if err != nil {
+		log.Error("Failed to load config", err)
+	}
+
+	r := gin.Default()
+
+	r.Run(c.Port)
+
+}
