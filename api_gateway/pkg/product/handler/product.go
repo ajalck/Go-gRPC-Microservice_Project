0c7a5ae3f5387b0e8b2ed9eeb7257b0a29619c03
Ajal C K <115230603+ajalck@users.noreply.github.com>
2023-05-16 16:51:44 +0530

    Product service client side created

--- api_gateway/pkg/product/handler/product.go
+++ api_gateway/pkg/product/handler/product.go
@@ -1,1 +1,70 @@
 package handler
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/ajalck/Go-gRPC-Microservice_Project/product_management/pkg/pb"
+	"github.com/gin-gonic/gin"
+)
+
+type ProductHandler struct {
+	PdtClient pb.ProductServiceClient
+}
+type CreateProductRequestBody struct {
+	ProductName string  `json:"product_name"`
+	Stock       int32   `json:"stock"`
+	Price       float32 `json:"price"`
+}
+
+func (h *ProductHandler) CreateProduct(ctx *gin.Context) {
+	body := &CreateProductRequestBody{}
+
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Inputs"})
+		return
+	}
+
+	res, err := h.PdtClient.CreateProduct(context.Background(), &pb.CreateProductRequest{
+		ProductName: body.ProductName,
+		Stock:       body.Stock,
+		Price:       body.Price,
+	})
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, err)
+		return
+	}
+	ctx.JSON(200, res)
+}
+func (h *ProductHandler) ListProduct(ctx *gin.Context) {
+	res, err := h.PdtClient.ListProduct(context.Background(), &pb.ListProductRequest{})
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, err)
+		return
+	}
+	ctx.JSON(200, res)
+}
+
+type UpdateProductRequestBody struct {
+	ProductId int32 `json:"product_id" gorm:"not null" binding:"required,numeric"`
+	Stock     int32 `json:"stock" gorm:"not null"`
+}
+
+func (h *ProductHandler) UpdateStock(ctx *gin.Context) {
+	body := &UpdateProductRequestBody{}
+
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	res, err := h.PdtClient.UpdateStock(context.Background(), &pb.UpdateStockRequest{
+		ProductId: body.ProductId,
+		Stock:     body.Stock,
+	})
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, err)
+		return
+	}
+	ctx.JSON(200, res)
+}
